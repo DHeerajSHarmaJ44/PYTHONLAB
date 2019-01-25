1992e9ecfefe516d6e6bf54fdc152e8d05347fcc
DHeerajSHarmaJ44 <47026240+DHeerajSHarmaJ44@users.noreply.github.com>
2019-01-25 14:56:23 +0530

    Add files via upload

---
+++ chess.py
@@ -0,0 +1,11 @@
+ 
+x=int(input("INPUT FIRST CO ORDINATE:  "	   ))
+y=int(input("input 2nd no :" ))
+a=int(input("INPUT 3RD NO. :" ))
+b=int(input (" input 4th no.: "    ))
+if x+y+a+b%2==0 :
+           print( "YES")
+else:
+    print("NO")
+
+
